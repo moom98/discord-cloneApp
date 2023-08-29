ac5f965c674072ac0d4df6805068cd4ecd552926
ymkmy98 <ymkmy98@gmail.com>
2023-08-30 00:22:23 +0900

    サイドバー構築

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import "./Sidebar.scss";
+import SidebarChannel from "./SidebarChannel";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import AddIcon from "@mui/icons-material/Add";
+import MicIcon from "@mui/icons-material/Mic";
+import HeadphonesIcon from "@mui/icons-material/Headphones";
+import SettingsIcon from "@mui/icons-material/Settings";
 
 export default function Sidebar() {
   return (
@@ -9,9 +15,41 @@ export default function Sidebar() {
           <img src="./assets/logo.svg" alt="" />
         </div>
       </div>
+
       <div className="sidebar__right">
         <div className="sidebar__top">
           <h3>discord</h3>
+          <ExpandMoreIcon />
+        </div>
+
+        <div className="sidebar__channels">
+          <div className="sidebar__channelsHeader">
+            <div className="sidebar__header">
+              <ExpandMoreIcon />
+              <h4>プログラミングチャンネル</h4>
+            </div>
+            <AddIcon className="sidebar__addChannel" />
+          </div>
+
+          <div className="sidebar__channelList">
+            <SidebarChannel />
+          </div>
+
+          <div className="sidebar__footer">
+            <div className="sidebar__account">
+              <img src="./vite.svg" alt="" />
+              <div className="sidebar__accountName">
+                <h4>moom98</h4>
+                <span>#8162</span>
+              </div>
+            </div>
+
+            <div className="sidebar__voice">
+              <MicIcon />
+              <HeadphonesIcon />
+              <SettingsIcon />
+            </div>
+          </div>
         </div>
       </div>
     </div>
---
+++ src/components/sidebar/SidebarChannel.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./SidebarChannel.scss";
+
+function SidebarChannel() {
+  return (
+    <div className="sidebarChannel">
+      <h4>
+        <span className="sidebarChannel__hash">#</span>
+        Udemy
+      </h4>
+      <h4>
+        <span className="sidebarChannel__hash">#</span>
+        Udemy
+      </h4>
+      <h4>
+        <span className="sidebarChannel__hash">#</span>
+        Udemy
+      </h4>
+    </div>
+  );
+}
+
+export default SidebarChannel;
